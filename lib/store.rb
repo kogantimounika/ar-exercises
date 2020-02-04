663ea462a35c2385e4a6c5fa3fc979dbba0ed85b
kogantimounika <kogantimounica@gmail.com>
2020-02-04 21:18:12 +0000

    added validation to store class

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validates :mens_apparel,  inclusion: { in: [true, false] }
+  validates :womens_apparel, inclusion: { in: [true, false] }
+  validate :mens_apperals_or_womens_apperals
+  def mens_apperals_or_womens_apperals
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:womens_apparel)
+    end
+  end
 end
+
+
