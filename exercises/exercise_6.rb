1d7d811445821a9e89b20a583548c1a9c99aa8fe
kogantimounika <kogantimounica@gmail.com>
2020-02-04 20:11:46 +0000

    Created employees tables and added some values

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mounika", last_name: "Koganti", hourly_rate: 70)
+@store1.employees.create(first_name: "Sai", last_name: "Krishna", hourly_rate: 80)
+
+@store1.employees.create(first_name: "Mouni", last_name: "Nuthalapati", hourly_rate: 60)
+@store1.employees.create(first_name: "Sri", last_name: "Lakshmi", hourly_rate: 70)
+@store1.employees.create(first_name: "Nani", last_name: "Krish", hourly_rate: 80)
